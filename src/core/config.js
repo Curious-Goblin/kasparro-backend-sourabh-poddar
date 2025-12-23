461ba4e80e93cfd047ff4673ba2eb30c2aab60e4
Sourabh Poddar <poddarsourabh9939@gmail.com>
2025-12-23 13:41:35 +0530

    docker set up completed

--- src/core/config.js
+++ src/core/config.js
@@ -5,13 +5,8 @@ dotenv.config();
 export const config = {
   port: process.env.PORT,
   databaseUrl: process.env.DATABASE_URL,
-  apiKey: process.env.API_KEY,
 };
 
 if (!config.databaseUrl) {
   throw new Error("DATABASE_URL is missing");
-}
-
-if (!config.apiKey) {
-  throw new Error("API_KEY is missing");
 }
